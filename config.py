7555459b8c6f563ee91b546bce286568451b58b7
Pedro Pena <pedrohenrique.pena@outlook.com>
2023-04-04 16:46:32 -0300

    Completando o crud

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+SECRET_KEY = os.urandom(12)
+
+SQLALCHEMY_DATABASE_URI = \
+    '{SGBD}://{usuario}:{senha}@{servidor}/{database}'.format(
+        SGBD = 'mysql+mysqlconnector',
+        usuario = 'root',
+        senha = 'root',
+        servidor = 'localhost',
+        database = 'jogoteca'
+    )
